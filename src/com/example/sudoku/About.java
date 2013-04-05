3fda2def5c74a1e7558f7ba41ede9ca4b08f3e37
Frank McAuley <fmcauley@resource.com>
2013-04-05 11:10:32 -0400

    Added the About button with labels and content. Also made updates to the MainActivity to start the About activity and listen for click events.

--- src/com/example/sudoku/About.java
+++ src/com/example/sudoku/About.java
@@ -1,8 +1,7 @@
 package com.example.sudoku;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class About extends Activity {
 
@@ -11,12 +10,4 @@ public class About extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.about, menu);
-		return true;
-	}
-
 }
--- src/com/example/sudoku/MainActivity.java
+++ src/com/example/sudoku/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.sudoku;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// Set up click listeners for all the buttons
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		View newButton = findViewById(R.id.new_button);
+		newButton.setOnClickListener(this);
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
+		
+	}
+	
+	public void onClick(View v){
+		switch (v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+		}
 	}
 
 	@Override
